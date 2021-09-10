72e85675af285e6fb454ee04de1a88c75474fd79
mkcyoung <m.c.young0@gmail.com>
2021-09-10 14:02:32 -0600

    implementing days / hours scheduling for people

--- src/components/AddPersonModal/AddPersonForm.tsx
+++ src/components/AddPersonModal/AddPersonForm.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
-// import { Formik, Form, Field, ErrorMessage } from "formik";
-// import { useFormik } from 'formik'
+import React, { useState } from 'react'
 import * as Yup from 'yup';
-// import { Grid, Button } from "semantic-ui-react";
 import { 
   Form, 
   Button,
   Space,
   Input,
   Row,
-  Col
+  Col,
+  Select,
+  TimePicker
 } from 'antd'
 import { nameRules } from './validationRules';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+const { Option } = Select;
 
 interface Props {
   onSubmit: () => void;
@@ -29,44 +30,76 @@ const validationSchema = Yup.object().shape({
     .required('Required'),
 });
 
+const dayOptions = [
+  {value: 'sunday', label:'Sunday'},
+  {value: 'monday', label:'Monday'},
+  {value: 'tuesday', label:'Tuesday'},
+  {value: 'wednesday', label:'Wednesday'},
+  {value: 'thursday', label:'Thursday'},
+  {value: 'friday', label:'Friday'},
+  {value: 'saturday', label: 'Saturday'}
+]
+
 const AddPersonForm = ({ onSubmit, onCancel } : Props ) => {
-  // const formik = useFormik({
-  //   initialValues: {
-  //     firstName: '',
-  //     lastName: ''
-  //   },
-  //   validationSchema: validationSchema,
-  //   // onSubmit: {onSubmit},
-  //   onSubmit: (values : any) => {
-  //     // same shape as initial values
-  //     console.log(values);
-  //   },
-  // });
+  const [form] = Form.useForm();
+  const [availableDays, setDays] = useState(dayOptions)
+
   const onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo);
   };
 
+  const handleDaySelection = () => {
+    const selectedDays = form.getFieldsValue().schedule.map((day:any)=> day ? day.day : '')
+    setDays(dayOptions.filter((dayObj)=> !selectedDays.includes(dayObj.value) ))
+  }
+
+  // const handleTimeSelection = (time : any, timeString : string[]) => {
+  //   console.log(time, timeString)
+  // }
+
   return (
     <Form
+      form={form}
       name='AddPersonForm'
       initialValues={{
         name: {
           firstName: '',
           lastName: ''
-        }
+        },
+        // schedule: [
+        //   {
+        //     day: 'Sunday',
+        //     startTime: '',
+        //     endTime: '',
+        //   }
+        // ],
       }}
-      labelCol={{ span: 4, offset: 3 }}
-      wrapperCol={{ span: 14 }}
       layout="horizontal"
       // requiredMark={'optional'}
-      onFinish={(values : any) => {
+      onFinish={(fieldsValues : any) => {
             // same shape as initial values
-            console.log(values);
+            console.log(fieldsValues);
+            const values = {
+              ...fieldsValues,
+              'schedule': fieldsValues.schedule.map((day: any) => {
+                return {
+                  ...day,
+                  time: {
+                    start: day.time.start.format('HH:mm'),
+                    end: day.time.end.format('HH:mm'),
+                  }
+                }
+              })
+            }
+            console.log(values)
       }}
       onFinishFailed={onFinishFailed}
     >
 
-      <Form.Item label='name'>
+      <Form.Item 
+      label='name'
+      labelCol={{ span: 4, offset: 3 }}
+      wrapperCol={{ span: 14 }}>
         <Space>
           <Form.Item
             // validateTrigger="onBlur"
@@ -90,63 +123,79 @@ const AddPersonForm = ({ onSubmit, onCancel } : Props ) => {
         </Space>
       </Form.Item>
 
+      <Form.Item
+      // labelCol={{ span: 3, offset: 4}}
+      // wrapperCol={{ span: 14, offset: 5}}
+      > 
+        <Form.List name="schedule">
+          {(fields, { add, remove }) => (
+            <>
+              {fields.map(({ key, name, fieldKey, ...restField }) => (
+                <Space key={key} align="baseline">
+                  <Form.Item
+                    label='day'
+                    {...restField}
+                    name={[name, 'day']}
+                    fieldKey={[fieldKey, 'day']}
+                    rules={[{ required: true, message: 'Missing day' }]}
+                  >
+                    <Select
+                      onChange={handleDaySelection}
+                      style={{ width: 120 }}
+                      allowClear={true}
+                    >
+                      {
+                        availableDays.map((day)=> (
+                          <Option key={day.value} value={day.value}> {day.label} </Option>
+                        ))
+                      }
+                    </Select>
+                  </Form.Item>
+                  <Form.Item 
+                    label='time'
+                    labelCol={{ span: 4, offset: 0 }}
+                    // wrapperCol={{ span: 14 }}
+                  >
+                    <Space align="baseline">
+                      <Form.Item
+                        {...restField}
+                        name={[name, 'time', 'start']}
+                        fieldKey={[fieldKey, 'start']}
+                        rules={[{ required: true, message: 'please select start time.' }]}
+                      >
+                        <TimePicker placeholder='start time' minuteStep={5} showNow={false} use12Hours format='h:mm a'/>
+                      </Form.Item>
+                      <Form.Item
+                        {...restField}
+                        name={[name, 'time', 'end']}
+                        fieldKey={[fieldKey, 'end']}
+                        rules={[{ required: true, message: 'please select end time' }]}
+                      >
+                        <TimePicker placeholder='end time' use12Hours minuteStep={5} showNow={false} format='h:mm a'/>
+                      </Form.Item>
+                      <MinusCircleOutlined onClick={() => remove(name)} />
+                    </Space>
+                  </Form.Item>
+                
+                </Space>
+              ))}
+              <Form.Item
+                wrapperCol={{ span: 14, offset: 5 }}>
+                <Button type="dashed" shape='round' onClick={() => add()} block icon={<PlusOutlined />}>
+                  add day
+                </Button>
+              </Form.Item>
+            </>
+          )}
+        </Form.List>
+      </Form.Item>
       
-      <div>
-        <Button htmlType='submit'>submit</Button>
-      </div>
-
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
     </Form>
-
-
-
-    // <Formik
-    //   initialValues={
-    //     {
-    //       firstName: '',
-    //       lastName: ''
-    //     }
-    //   }
-    //   onSubmit={values => {
-    //     // same shape as initial values
-    //     console.log(values);
-    //   }}
-    //   validationSchema={validationSchema}
-    // >
-    //   {({ isValid, dirty, values, errors }) => (
-    //      <Form>
-    //        <Field 
-    //         name='firstName'
-    //         label='first name'
-    //         placeholder='first name'
-
-    //       />
-    //        <ErrorMessage name="firstName" />
-    //        <Field 
-    //         name="lastName" 
-    //         label="last name"
-    //         placeholder='last name'
-    //       />
-    //        <ErrorMessage name="lastName" />
-    //        <Grid>
-    //             <Grid.Column floated="left" width={5}>
-    //               <Button type="button" onClick={onCancel} color="red">
-    //                 Cancel
-    //               </Button>
-    //             </Grid.Column>
-    //             <Grid.Column floated="right" width={5}>
-    //               <Button
-    //                 type="submit"
-    //                 floated="right"
-    //                 color="green"
-    //                 disabled={!dirty || !isValid}
-    //               >
-    //                 Add
-    //               </Button>
-    //             </Grid.Column>
-    //           </Grid>
-    //      </Form>
-    //    )}
-    // </Formik>
   )
 }
 
