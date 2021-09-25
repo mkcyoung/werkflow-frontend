ceddde3efadec47df0015aa1b4541ac09ef29b0e
mkcyoung <m.c.young0@gmail.com>
2021-09-25 12:52:49 -0600

    working on task card styling

--- src/components/Schedule/CalendarBody.tsx
+++ src/components/Schedule/CalendarBody.tsx
@@ -5,6 +5,7 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import { styled } from '@mui/material/styles';
+import TaskCard from './TaskCard';
 
 type Props = {
     taskData: Task[];
@@ -26,41 +27,114 @@ const CalendarBody = ({taskData, week}: Props) => {
         .map(task => task.category)
         .filter(onlyUnique) : []
 
+    // Create an object that represents the week, this may make populating the schedule easier
+    // plus I can assign people to each task, then I can save this object to the backend to be 
+    // retrieved later, I think that's how I'll populate the chart....
+    // Maybe it makes sense here to make this one big form using formik...
+    const weekObj: any = {
+        week: week[0] ? format(week[0].date,'I') : null
+    }
+    week.map((weekDay) => {
+        const tasksOnDay = taskData.filter((task) => task.schedule.map(day => day.day).includes(format(weekDay.date, 'EEEE').toLowerCase()))
+        weekObj[`${weekDay.formatted}`] = {
+            day: weekDay.date,
+            tasks: tasksOnDay.map((task) => {
+                const taskDaySchedule = task.schedule.filter((day) => day.day === format(weekDay.date, 'EEEE').toLowerCase())[0]
+                return {
+                    name: task.name,
+                    category: task.category,
+                    people: task.people,
+                    taskTime: task.taskTime,
+                    taskList: taskDaySchedule.subTasks ? taskDaySchedule.subTasks.map((subTask: any) => {
+                        return {
+                            taskName: `${task.name} @ ${subTask.start}`,
+                            person: null
+                        }
+                    }) : 
+                    [{
+                        taskName: `${task.name}`,
+                        person: null
+                    }]
+                }
+            })
+        }
+    })
+    
+    console.log("Week Obj",weekObj)
+
     return (
         <Grid container spacing={1} columns={7} sx={{paddingTop:'10px'}}>
             {
-                taskCategories.map((category, index) => {
-                    const tasksInCategory = taskData.filter((task) => task.category === category)
+                taskCategories.map((category,index) => {
                     return (
                         <Grid key={index} container item spacing={1}>
                             <Grid item xs={7}> 
-                                <Item> {category}</Item>
+                                <Item>{category}</Item>
                             </Grid>
-                            {
-                                week.map((weekDay, index) => {
-                                    const tasksOnDay = tasksInCategory.filter((task) => task.schedule.map(day => day.day).includes(format(weekDay.date, 'EEEE').toLowerCase()))
-                                    if (tasksOnDay.length > 0 ) {
-                                        return tasksOnDay.map((task) => {
-                                                console.log(task)
-                                                return (
-                                                    <Grid item key={index} xs={1}>
-                                                        <Item>{task.name}</Item>
-                                                    </Grid>
-                                                    )
-                                                })
-                                    } else {
-                                        return (
-                                            <Grid item key={index} xs={1}>
-                                                <Item> </Item>
-                                            </Grid>
-                                            )
-                                    }
-                                })
-                            }
+                        
+                        {
+                            week.map((weekDay, i) => {
+                                const tasksOnDay = weekObj[`${weekDay.formatted}`].tasks?.filter((task: any) => task.category === category)
+                                if (tasksOnDay.length > 0){
+                                    return tasksOnDay.map((task: any) => {
+                                        return <TaskCard key={i} task={task} weekDay={weekDay} />
+                                        // return task.taskList.map((subTask: any, j: any) => {
+                                        //     return (
+                                        //         <Grid item key={`${i}-${j}`} xs={1}>
+                                        //             <Item>{subTask.taskName}</Item>
+                                        //         </Grid>
+                                        //     )
+                                        // })
+
+                                    })
+                                } else {
+                                    return (
+                                        <Grid item key={i} xs={1}>
+                                            <Item></Item>
+                                        </Grid>
+                                    )
+                                }
+                            })
+                        }
                         </Grid>
-                )})
+                    )
+                })
             }
         </Grid>
+        // <Grid container spacing={1} columns={7} sx={{paddingTop:'10px'}}>
+        //     {
+        //         taskCategories.map((category, index) => {
+        //             const tasksInCategory = taskData.filter((task) => task.category === category)
+        //             return (
+        //                 <Grid key={index} container item spacing={1}>
+        //                     <Grid item xs={7}> 
+        //                         <Item> {category}</Item>
+        //                     </Grid>
+        //                     {
+        //                         week.map((weekDay, index) => {
+        //                             const tasksOnDay = tasksInCategory.filter((task) => task.schedule.map(day => day.day).includes(format(weekDay.date, 'EEEE').toLowerCase()))
+        //                             if (tasksOnDay.length > 0 ) {
+        //                                 return tasksOnDay.map((task) => {
+        //                                         console.log(task)
+        //                                         return (
+        //                                             <Grid item key={index} xs={1}>
+        //                                                 <Item>{task.name}</Item>
+        //                                             </Grid>
+        //                                             )
+        //                                         })
+        //                             } else {
+        //                                 return (
+        //                                     <Grid item key={index} xs={1}>
+        //                                         <Item> </Item>
+        //                                     </Grid>
+        //                                     )
+        //                             }
+        //                         })
+        //                     }
+        //                 </Grid>
+        //         )})
+        //     }
+        // </Grid>
     )
 }
 
---
+++ src/components/Schedule/TaskCard.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {format} from 'date-fns';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import { WeekDayObj } from '../../types';
+
+type Props = {
+    task: any,
+    weekDay: WeekDayObj
+}
+const TaskCard = ({task, weekDay} : Props) => {
+    console.log("Tasks:",task,weekDay.day)
+    // Alternatively here could get people from state based on IDs.... probably should do this.
+    const peopleOnDay = task.people.filter((person: any) => person.schedule.map((day: any) => day.day).includes(format(weekDay.date, 'EEEE').toLowerCase()))
+    console.log("people available for task",peopleOnDay)
+    // TODO: Check here to see if they have the day off 
+    // TODO: Implement sorting here based on that person's time for the day
+    // TODO: Need to figure out what state to use and how that state interacts with the database storage
+    // TODO: Make task cards responsive and size efficient
+    return (
+        <Grid item xs={1}>
+            <Card sx={{ display: 'flex' }}>
+                <Box sx={{ display: 'flex', flexDirection: 'row', flex: '1 1 auto' }}>
+                    <CardContent sx={{flex: '1 1 auto'}}>
+                        <Stack spacing={0.5} sx={{flex: 1}}>
+                        {
+                            task.taskList.map((subTask: any, index: any) => {
+                                return (
+                                    <Autocomplete
+                                        multiple
+                                        key={index}
+                                        size='small'
+                                        id={`${weekDay.formatted}-subTask.taskName`}
+                                        freeSolo
+                                        options={peopleOnDay.map((person: any) => person.name.first)}
+                                        // getOptionLabel={(person: any) => person.name.first}
+                                        renderInput={(params) => <TextField {...params} 
+                                        // InputLabelProps={{style: {fontSize: 18}}} 
+                                        label={subTask.taskName} />}
+                                    />
+                                       
+                                )
+                            })
+                        }
+                        </Stack>
+                    </CardContent>
+                </Box>
+            </Card>
+        </Grid>
+    )
+}
+
+
+export default TaskCard
